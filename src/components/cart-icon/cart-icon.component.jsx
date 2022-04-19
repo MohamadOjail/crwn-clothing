32f6a420eb7e3998427da86869474a11f3964f2d
Mohamad Ojail <mohamad.ojail@yaelev.se>
2022-04-20 00:29:59 +0200

    section__9__steps__116_to_119__Add-To-Cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,19 +1,22 @@
-import './cart-icon.styles.scss';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { useContext } from 'react';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
 import { CartContext } from '../../contexts/cart.context';
 
-const CartIcon = () =>{
+import './cart-icon.styles.scss';
 
-    const {isCartOpen, setIsCartOpen } = useContext(CartContext);
-    const toggleIsCartOpen = ()=> setIsCartOpen(!isCartOpen);
+const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen, cartItemCount } = useContext(CartContext);
 
-    return(
-        <div className='cart-icon-container' onClick={toggleIsCartOpen}>
-            <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>0</span>
-        </div>
-    )
-}
+  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
+  return (
+    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+      <ShoppingIcon className='shopping-icon' />
+      <span className='item-count'>{cartItemCount}</span>
+    </div>
+  );
+};
 
 export default CartIcon;
