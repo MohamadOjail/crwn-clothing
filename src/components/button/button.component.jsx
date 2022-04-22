858815b8e60970a0179043bdf40a3450f2f59008
Mohamad Ojail <mohamad.ojail@yaelev.se>
2022-04-22 22:55:05 +0200

    section__11__137_styled_components_cart_dropdown

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,27 +1,25 @@
-import { BaseButton, GoogleSignInButton, InvertedButton } from "./button.styles";
-
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from "./button.styles";
 
 export const BUTTON_TYPES_CLASSES = {
-    base: 'base',
-    google: 'google-sign-in',
-    inverted: 'inverted',
-}
+  base: "base",
+  google: "google-sign-in",
+  inverted: "inverted",
+};
 
-const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) =>(
-    {
-        [BUTTON_TYPES_CLASSES.base]: BaseButton,
-        [BUTTON_TYPES_CLASSES.google]: GoogleSignInButton,
-        [BUTTON_TYPES_CLASSES.inverted]: InvertedButton,
-    }[buttonType]
-)
+const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) =>
+  ({
+    [BUTTON_TYPES_CLASSES.base]: BaseButton,
+    [BUTTON_TYPES_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPES_CLASSES.inverted]: InvertedButton,
+  }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps })=>{
-    const TheButton = getButton(buttonType);
-    return(
-        <TheButton {...otherProps}>
-            {children}
-        </TheButton>
-    )
-}
+const Button = ({ children, buttonType, ...otherProps }) => {
+  const TheButton = getButton(buttonType);
+  return <TheButton {...otherProps}>{children}</TheButton>;
+};
 
 export default Button;
