056b11a18ca09b79dda6d86c1eb0f19b0dc21b13
Mohamad Ojail <mohamad.ojail@yaelev.se>
2022-04-22 22:36:21 +0200

    section__11__136_styled_components_button

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -6,7 +6,7 @@ import {
 } from "../../util/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import "./sign-in-form.styles.scss";
-import Button from "../button/button.component";
+import Button, {BUTTON_TYPES_CLASSES} from "../button/button.component";
 
 const defaultFormFields = {
   email: "",
@@ -79,7 +79,7 @@ const SignInForm = () => {
 
         <div className="buttons-container">
           <Button type="submit">Log in</Button>
-          <Button type='button' buttonType="google" onClick={signInWithGoogle}>
+          <Button type='button' buttonType={BUTTON_TYPES_CLASSES.google} onClick={signInWithGoogle}>
             Google sign in
           </Button>
         </div>
